1966e16fffc7ea4f6f72bcaa791260618a836bd2
Vojtech Mares <iam@vojtechmares.com>
2021-04-07 20:01:49 +0200

    feat(app): added version info

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
 func main() {
 	fmt.Println("Hello GoReleaser")
+	fmt.Printf("Version: %s, commit: %s, date: %s, by: %s\n", version, commit, date, builtBy)
 }
